ae6849e88cbf4e7f9834a7a376e2ed52513d0940
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-02-04 15:58:33 -0600

    setup basic components to display data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :pokemons
-  resources :trainers
+  resources :pokemons, only: [:index, :show, :create, :destroy]
+  resources :trainers, only: [:index, :show]
   resources :users, only: [:index, :show]
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 puts "Seeding data **************"
 user = User.create(username: "Cristian", password_digest: "1234")
+user2 = User.create(username: "Alex", password_digest: "54321")
+
 trainer1 = Trainer.create(name: "Cris", party_count: 1, user_id: user.id)
-trainer2 = Trainer.create(name: "Abs", party_count: 1, user_id: user.id)
+trainer2 = Trainer.create(name: "Abs", party_count: 1, user_id: user2.id)
 
 pokemon1 = Pokemon.create(
     name: "Bulbasaur", 
-    types: "Grass", 
+    types: ["Grass", "Poison"] 
     url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", 
     trainer_id: trainer1.id
 )
