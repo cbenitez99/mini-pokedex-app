a606acb84c3d0011c54b45aceecc0cb4f17e22c8
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-02-05 00:59:11 -0600

    Set up for login

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
-  <React.StrictMode>
-  <App />
-
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>,
   document.getElementById("root")
 );
