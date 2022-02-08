6ea5ed19ff845756f7cf5bd5d26ea8cdb9e050f0
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-02-08 13:39:11 -0600

    Save User State: in progress , Navbar: in progress, Session/User Controllers: working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import PokemonContainer from "./components/PokemonContainer";
 import AddPokemonForm from "./components/AddPokemonForm";
 import TrainersContainer from "./components/TrainersContainer";
@@ -7,10 +7,27 @@ import PokemonEditForm from "./components/PokemonEditForm";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
 import Home from "./components/Home";
-
 import Navbar from "./components/Navbar";
-function App() {
+import axios from "axios"
+
+const App = () => {
   const [user, setUser] = useState(null)
+  const [pokemon, setPokemon] = useState("bulbasaur")
+  const [pokemonData, setPokemonData] = useState([])
+  const [pokemonType, setPokemonType] = useState("")
+
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const res = await axios.get(url);
+      toArray.push(res.data);
+      setPokemonType(res.data.types[0].type.name)
+      console.log(res)
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <div>
       <Navbar user={user}/>
