c9c28ffdd9673f2f4486735842b801ef0dd353a0
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-03-23 13:10:28 -0500

    adding css

--- client/src/components/FindPokemon.js
+++ client/src/components/FindPokemon.js
@@ -2,6 +2,7 @@ import "../App.css"
 import React, {useState} from "react"
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import PokemonStats from "./PokemonStats";
 
 const FindPokemon = ({user}) => {
     let navigate = useNavigate();
@@ -83,60 +84,16 @@ const FindPokemon = ({user}) => {
                     Search
                 </span>
             </button>
-        </form>
-        
-        {pokemonData.map((data) => {
-        return (
-            <>
-            <div key={data.id} className="container">
-                <div className="divTable">
-                <img className="sprite" src={data.sprites["front_default"]} alt="poke-sprite"/>
-                <div className="divTableBody">
-                <p style={{color: "red"}}>{errors}</p> 
-                    <div className="divTableRow">
-                        <div className="divTableCell">ID</div>
-                        <div className="divTableCell">{data.id}</div>
-                    </div>
-                    <div className="divTableRow">
-                        <div className="divTableCell">Name</div>
-                        <div className="divTableCell">{data.name.toUpperCase()}</div>
-                    </div>
+        </form> 
+        <div class="circle"></div><div class="circle"></div>
+        <div class="triangle-left"></div>
+        <div class="triangle-up"></div>
+        <div class="triangle-right"></div>
+        <div class="triangle-down"></div>
 
-                    <div className="divTableRow">
-                        <div className="divTableCell">Type</div>
-                        <div className="divTableCell">{data.types.map((type)=>type.type.name.toUpperCase()).join(" / ")}</div>
-                    </div>
 
-                    <div className="divTableRow">
-                        <div className="divTableCell">HP</div>
-                        <div className="divTableCell">❤️{data.stats[0].base_stat}</div>
-                    </div>
-                    <div className="divTableRow">
-                        <div className="divTableCell">Height</div>
-                        <div className="divTableCell">
-                            {" "}
-                            {Math.round(data.height * 3.9)}"
-                        </div>
-                    </div>
-                    <div className="divTableRow">
-                        <div className="divTableCell">Weight</div>
-                        <div className="divTableCell">
-                            {" "}
-                            {Math.round(data.weight / 4.3)} LBS
-                        </div>
-                    </div>
-            
-                    </div>
-                </div>
-            </div><br/>
-            <button className="button-82-pushable" onClick={handleCaptureClick}>
-                <span className="button-82-shadow"></span>
-                <span className="button-82-edge"></span>
-                <span className="button-82-front text">Capture</span>
-            </button>
-        </>
-        );
-        })}
+        {<PokemonStats pokemonData={pokemonData} errors={errors} handleCaptureClick={handleCaptureClick}/>}
+
         <br/>
     </div>
     
--- client/src/components/PokemonStats.js
+++ client/src/components/PokemonStats.js
@@ -1,37 +1,62 @@
 import React from 'react'
 
-const PokemonStats = ({pokemonData}) => {
+const PokemonStats = ({pokemonData, errors, handleCaptureClick}) => {
     console.log(pokemonData)
 
-    let stats = pokemonData.stats.map((stat) => <div className="stats-table">
-    {/* <div className="divTableRow">
-        <div className="divTableCell">Pokedex#</div>
-        <div className="divTableCell">{stat.}</div>
-    </div>
-
-    <div className="divTableRow">
-        <div className="divTableCell">HP</div>
-        <div className="divTableCell">❤️{stat.}</div>
-    </div>
-
-    <div className="divTableRow">
-        <div className="divTableCell">Height</div>
-        <div className="divTableCell">
-            {Math.round(stat.height * 3.9 )+ `"`}
-        </div>
-    </div>
-
-    <div className="divTableRow">
-        <div className="divTableCell">Weight</div>
-        <div className="divTableCell">
-            {Math.round(stat.weight / 4.3)} lbs
-        </div>
-    </div> */}
-
-</div>)
     return (
-        <div >
-            {stats}
+        <div className="mini-pokemon-form">
+            {pokemonData.map((data) => {
+        return (
+            <>
+            <div key={data.id} className="container">
+                <div className="divTable">
+                <img className="sprite" src={data.sprites["front_default"]} alt="poke-sprite"/>
+                <div className="divTableBody">
+                <p style={{color: "red"}}>{errors}</p> 
+                    <div className="divTableRow">
+                        <div className="divTableCell">ID</div>
+                        <div className="divTableCell">{data.id}</div>
+                    </div>
+                    <div className="divTableRow">
+                        <div className="divTableCell">Name</div>
+                        <div className="divTableCell">{data.name.toUpperCase()}</div>
+                    </div>
+
+                    <div className="divTableRow">
+                        <div className="divTableCell">Type</div>
+                        <div className="divTableCell">{data.types.map((type)=>type.type.name.toUpperCase()).join(" / ")}</div>
+                    </div>
+
+                    <div className="divTableRow">
+                        <div className="divTableCell">HP</div>
+                        <div className="divTableCell">❤️{data.stats[0].base_stat}</div>
+                    </div>
+                    <div className="divTableRow">
+                        <div className="divTableCell">Height</div>
+                        <div className="divTableCell">
+                            {" "}
+                            {Math.round(data.height * 3.9)}"
+                        </div>
+                    </div>
+                    <div className="divTableRow">
+                        <div className="divTableCell">Weight</div>
+                        <div className="divTableCell">
+                            {" "}
+                            {Math.round(data.weight / 4.3)} LBS
+                        </div>
+                    </div>
+            
+                    </div>
+                </div>
+            </div><br/>
+            <button className="button-82-pushable" onClick={handleCaptureClick}>
+                <span className="button-82-shadow"></span>
+                <span className="button-82-edge"></span>
+                <span className="button-82-front text">Capture</span>
+            </button>
+        </>
+        );
+        })}
         </div>
     );
 }
