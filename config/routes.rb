72cdee1ec5fef941584e50534c5c7f2225932003
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-05-20 19:32:38 -0500

    refactored rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   resources :stats
-  resources :moves, only: [:index, :show, :create]
-  resources :pokemons, only: [:index, :show, :create]
+  resources :moves, only: [:index, :show, :create, :update, :destroy]
+  resources :pokemons, only: [:index, :show, :create, :update, :destroy]
   resources :users, only: [:show, :create]
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  delete "/pokemons/:id", to: "pokemons#destroy"
-  patch "/pokemons/:id", to: "pokemons#update"
-  patch "/moves/:id", to: "moves#update"
-  delete "/moves/:id", to: "moves#destroy"
 end
