4be15b79c558e4c33b767be6120d4a8a3983b4a1
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-05-21 20:53:50 -0500

    added embedded video to home page, tweaked css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,13 +8,19 @@ const Home = ({user}) => {
     navigate(`/users/${user.id}`)
   }
 
+  const handleLoginClick = () => {
+    navigate(`/login`)
+  }
+  const handleSignupClick = () => {
+    navigate(`/signup`)
+  }
+
+
   if (!!user.id) {
     return (
       <div className='home-page'>
         <h3>Welcome to the Mini Pokedex, {user.username}!</h3>
-         <iframe width="420" height="315"
-        src="https://www.youtube.com/embed/rg6CiPI6h2g">
-        </iframe>
+        <iframe className="embed-vid" src="https://www.youtube.com/embed/rg6CiPI6h2g"/>
         <p>This program allows a trainer to search for and catch their favorite Pokemon! To get started, head over to your</p><div className="direct-profile" onClick={handleClick} style={{color: "red", fontSize: 20}}>Trainer Profile!</div>
         <p className="creator-text">
         ◓ <a href="https://github.com/cbenitez99">GitHub</a> | <a href="https://cbenitez99.medium.com">Blogs</a> ◓
@@ -25,10 +31,9 @@ const Home = ({user}) => {
     return (
       <div className='home-page-logout'>
         <h1>Welcome to the Mini Pokedex!</h1>
-         <iframe width="420" height="315"
-        src="https://www.youtube.com/embed/rg6CiPI6h2g">
-        </iframe>
-        <h3>Please Log-In or Sign-Up First</h3>
+         <iframe className="embed-vid" src="https://www.youtube.com/embed/rg6CiPI6h2g"/>
+        <h3>Please <a className="direct-profile" onClick={handleLoginClick} style={{color: "red", fontSize: 20, margin: 0}}>Log-In</a> or <a className="direct-profile" onClick={handleSignupClick} style={{color: "red", fontSize: 20}}>Sign-Up</a> First</h3>
+        
         <p className="creator-text">
         ◓ <a href="https://github.com/cbenitez99">GitHub</a> | <a href="https://cbenitez99.medium.com">Blogs</a> ◓
         </p>
