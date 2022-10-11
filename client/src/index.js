3299b8503abb11691933f4c95b2745506e73237d
Cristian Benitez <cbenitezbusiness9@gmail.com>
2022-10-11 15:13:04 -0500

    updating routing v18

--- client/src/index.js
+++ client/src/index.js
@@ -12,3 +12,8 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('root');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="root" />);
